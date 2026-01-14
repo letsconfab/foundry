98dedccedf33d5098c3e4932c9ea4380e91d1ee1
Guinnes Singh <gswebspace@gmail.com>
2026-01-14 09:00:49 +0530

    Added foundry host to allowed hosts in vite

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,5 +1,4 @@
-
-  import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
   import react from '@vitejs/plugin-react-swc';
   import path from 'path';
 
@@ -56,5 +55,13 @@
     server: {
       port: 3000,
       open: true,
+      host: true,
+      allowedHosts: ['localhost:3002', 'foundry.letsconfab.org'],
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3002',
+          changeOrigin: true,
+        }
+      }
     },
   });
